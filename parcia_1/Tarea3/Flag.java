463e45c27eef7175d13222949919b53f8ba863e4
JoseLuisTD1104 <luisyteclados.jl@gmail.com>
2022-10-04 23:08:44 -0500

    agregacion de archivos java

---
+++ parcia_1/Tarea3/Flag.java
@@ -0,0 +1,19 @@
+/*
+
+*Flag.java
+*Author: JoseLuis1104
+*Date: oct 4,2022
+Location:  Mexico
+
+*/
+public class Flag { // name of program
+
+  public static void main(String[] args) { // main executable
+    
+    boolean flag = true; // define a boolean var
+    System.out.println("valor de flag: "+!!flag+"\n");  //print flag 
+   
+  }
+
+    
+}
---
+++ parcia_1/Tarea3/Range.java
@@ -0,0 +1,25 @@
+/* 
+
+Flag.java
+Author: JoseLuis1104
+Date: oct 4,2022
+Location:  Mexico
+
+ */
+
+public class Range { // name of program
+   
+    static byte valbyte = 100; // define a byte var
+    static short valshort = 1024; // define a short var
+    static int range = 4250000;  // define a range var
+
+    public static void main(String[] args) {  // main executable
+        
+        System.out.println("byte : \n"+ valbyte +"\n"); //print valbyte
+        System.out.println("short : \n"+ valshort +"\n"); // //print valshort
+        System.out.println("int : \n"+ range +"\n"); //print range  
+
+       
+    }
+
+}
