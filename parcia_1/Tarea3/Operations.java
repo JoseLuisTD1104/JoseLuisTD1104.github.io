5e6bda2a181574b133af2b98222a5b9d19177773
JoseLuisTD1104 <luisyteclados.jl@gmail.com>
2022-10-10 08:07:19 -0500

    add file Operations

---
+++ parcia_1/Tarea3/Operations.java
@@ -0,0 +1,36 @@
+/* 
+
+Operations.java
+Author: JoseLuis1104
+Date: oct 10,2022
+Location:  Mexico
+
+*/
+
+public class Operations {
+    public static void main(String[] args) {
+      
+      // declare variables
+      double a = 20, b = 5.32;
+      double c = 0;
+  
+      // addition operator
+      System.out.println ("(a + b) / 2 = ");
+      c = (a * b) / 2;
+      System.out.println (c);
+  
+      // subtraction operator
+      System.out.println("a - b = " + (a - b));
+  
+      // multiplication operator
+      System.out.println("a * b = " + (a * b));
+  
+      // division operator
+      System.out.println("a / b = " + (a / b));
+  
+      // modulo operator
+      System.out.println("a % b = " + (a % b));
+      
+    }
+  }
+  
